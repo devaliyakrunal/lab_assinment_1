fe8ae034d9ef5de77703383dab0ff44c5ef3fe96
devaliyakrunal <bca2022krunal1747@tnraocollege.org>
2024-07-14 21:26:16 +0530

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,12 @@
+num1=float(input("enter a first number:"))
+num2=float(input("enter a second number:"))
+
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+
+print("addition:",add)
+print("subtraction:",sub)
+print("multification:",mul)
+print("divison:",div)
